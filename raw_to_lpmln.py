2043291af5c73f01db1b3548eae7ad412c02269a
anishpradhan <mail.anishpradhan@gmail.com>
2018-03-07 11:52:17 -0700

    rule converter

--- raw_to_lpmln.py
+++ raw_to_lpmln.py
@@ -1,27 +1,43 @@
 import argparse
 import sys
+import re
+import csv
 
 
-def rule_parser(fname):
+def rule_parser_amie(fname, predicate):
     with open(fname) as f:
         content = f.readlines()
-    for con in content:
-        print con
+    rule_list = []
+    for it,con in enumerate(content):
+        relation = re.findall(r":(.*?)\>", con)
+        if relation.index(predicate) != 0:
+            new_index = relation.index(predicate)
+            temp = relation[0]
+            relation[0] = predicate
+            relation[new_index] = temp
+        vars = re.findall(r"\?(.)", con)
+        score = re.findall(r"\d.\d+", con)
+        vars = [var.upper() for var in vars]
         i = 0
-        pred = ''
-        for c in con:
-            if c == "?":
-                x = con[i+1]
-                print x
-            if c == ":":
-                pred = ''
-                i+=1
-                while con[i] != ">":
-                    pred += con[i]
-                    i+=1
-                print pred
-            i+=1
-        sys.exit()
+        rule = str(score[0]+' ')
+        for rel in relation:
+            rule += rel +'('+vars[i]+','+ vars[i+1]+')'
+            if i == 0:
+                rule += ' :- '
+            elif i == len(relation)-1:
+                rule += '.'
+            else:
+                rule += ' , '
+            i += 1
+        rule_list.append(rule)
+    return rule_list
+
+
+def rule_writer(rule_list, predicate, rule_type, folder_path):
+    with open(folder_path+predicate+"_"+rule_type+".csv", 'wb') as resultFile:
+        wr = csv.writer(resultFile,quoting = csv.QUOTE_NONE,escapechar=' ')
+        for rule in rule_list:
+            wr.writerow([rule,])
 
 
 if __name__ == "__main__":
@@ -29,5 +45,8 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--test_predicate", default='sample_case')
     parser.add_argument("-r", "--rule_type", default='amie')
     args = parser.parse_args()
+    folder_path = 'dataset/'+args.test_predicate+'/rules/'+args.rule_type+'/'
     path = 'dataset/'+args.test_predicate+'/rules/'+args.rule_type+'/'+args.test_predicate+'.csv'
-    rule_parser(path)
+    if args.rule_type=='amie':
+        rule_list = rule_parser_amie(path, args.test_predicate)
+    rule_writer(rule_list, args.test_predicate,args.rule_type, folder_path)
