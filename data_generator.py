f700168ba51ff181f169c040c0e9f59325239169
anishpradhan <mail.anishpradhan@gmail.com>
2018-04-06 18:39:11 -0700

    negative test

--- data_generator.py
+++ data_generator.py
@@ -21,7 +21,7 @@ def data_reader(folder_path):
 def write_samples(folder_path, i, true_data, false_data):
     with open(folder_path+"sample_"+str(i)+".csv", 'wb') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['id', 'sub', 'obj', 'class'])
+        writer.writerow(['sid', 'sub', 'obj', 'class'])
         j = 1
         for td in true_data:
             writer.writerow([j,td[0],td[1],1])
@@ -38,15 +38,15 @@ if __name__ == "__main__":
     folder_path = 'dataset/'+args.test_predicate+'/input/'
     data_1, data_2, true_data = data_reader(folder_path)
     catesian_data = []
-    false_data = []
+
     true_sample = []
-    for element in itertools.product(data_1,data_2):
+    for element in itertools.product(data_1, data_2):
         catesian_data.append(element)
     for i in range(3):
-        i = i+3
-        sample = random.sample(catesian_data, 400)
+        false_data = []
+        sample = random.sample(catesian_data, 30)
         for sam in sample:
-            if sam not in true_data and len(false_data) < 199:
+            if sam not in true_data and len(false_data) < 16:
                 false_data.append(sam)
             else:
                 true_sample.append(sam)
